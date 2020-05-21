231c29baaba038a221c7bb2f870b1c7675832784
Akash Mandal <65537334+geeky-mandy@users.noreply.github.com>
2020-05-22 03:28:28 +0530

    Added Random color generator
    colors array can now generate random rgb color & h1-background gets colored on selecting the correct color.

--- colorgame/script.js
+++ colorgame/script.js
@@ -1,16 +1,10 @@
-var colors = [
-			"rgb(255, 0, 0)",
-			"rgb(255, 255, 0)",
-			"rgb(0, 255, 0)",
-			"rgb(0, 255, 255)",
-			"rgb(0, 0, 255)",
-			"rgb(255, 0, 255)"	
-];
+var colors = generateRandomColor(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickRandomColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -29,6 +23,7 @@ for(var i=0; i<squares.length; i++)
 			{
 				messageDisplay.textContent = "Correct";
 				sameColor(clickedColor);
+				h1.style.background = pickedColor;
 			}
 		else
 			{
@@ -49,4 +44,26 @@ function pickRandomColor()
 {
 	var random = Math.floor(Math.random() * colors.length);
 	return(colors[random]);
+}
+function generateRandomColor(num)
+{
+	//make an empty array
+	var arr =[];
+	//loop over the number of elements in the arguement
+	for(var i=0; i<num; i++)
+	{
+		arr.push(randomRgb());
+	}
+	return arr;
+}
+function randomRgb()
+{
+ 	//random r 0-255
+ 	var r = Math.floor(Math.random() *256);
+ 	//random g 0-255
+ 	var g = Math.floor(Math.random() *256);
+ 	//random b 0-255
+ 	var b = Math.floor(Math.random() *256);
+ 	//return rgb in the format of "rgb(255, 0, 0)"
+ 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
