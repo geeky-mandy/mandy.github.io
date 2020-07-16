35ce6bbae6b0110891f156eb363bfd948a65e220
Akash Mandal <65537334+geeky-mandy@users.noreply.github.com>
2020-07-17 02:59:06 +0530

    navbar - expand- working

--- video-us/main.js
+++ video-us/main.js
@@ -1,27 +1,3 @@
 $(document).ready(function () {
-    $(".insert").hide();
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll != 0) {
-            $(".navbar-light").addClass("navbar-scroll");
-        }
-        if (scroll > 1200) {
-            $(".insert").show();
-            $(".upper-video").hide();
-        }
-        if (scroll == 0) {
-            $(".insert").hide();
-            $(".upper-video").show();
-        }
-        else {
-            $(".navbar-light").removeClass("navbar-scroll");
-        }
-    })
-    $(window).scroll(function () {
-        var scrol = $(window).scrollTop();
-        if (scrol > 1200) {
-            $(".insert").show();
-            $(".upper-video").hide();
-        }
-    })
+
 });
